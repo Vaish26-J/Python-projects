c37258a634d5337697eac063bf18f1c5c71d9033
Vaish26-J <56188118+Vaish26-J@users.noreply.github.com>
2021-02-27 10:56:00 +0530

    Delete main.py

--- main.py
+++
@@ -1,8 +0,0 @@
-import random,string
-name = input('enter a name').replace(' ','')
-dob = input('enter date of birth in the format dd/mm/yyyy').strip().split('/')
-print(dob)
-password = []
-extractint = random.randint(0,5)
-extractdate = random.randint(0,2)
-for i in name
